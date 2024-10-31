57da4fa5bdeb2c23710d41f9df5e0e8835dc271b
xavis <xavis@xavSagum23.dhcp.wustl.edu>
2024-10-31 10:23:35 -0500

    working on rectangle

---
+++ src/studio7/Rectangle.java
@@ -0,0 +1,28 @@
+package studio7;
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Rectangle {
+	private double length;
+	private double width;
+	private double area;
+	private double perimeter;
+
+	public Rectangle(double length, double width, double area, double perimeter);{
+		length = initLength;
+		width = initWidth;
+		area = initArea;
+		perimeter = initPerimeter;
+	}
+	
+	public static void draw() {
+		filledRectangle(length, width, )
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
